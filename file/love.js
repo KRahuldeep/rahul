34c9b29600cf52d85d1c7d5c6a6d3956cfb0bf75
KRahuldeep <168513400+KRahuldeep@users.noreply.github.com>
2026-06-12 04:12:29 +0530

    Update love.js

--- file/love.js
+++ file/love.js
@@ -52,18 +52,23 @@
     }
 
     Cake = function() {
-        // x = 16 sin^3 t
-        // y = 13 cos t - 5 cos 2t - 2 cos 3t - cos 4t
-        // http://www.wolframalpha.com/input/?i=x+%3D+16+sin%5E3+t%2C+y+%3D+(13+cos+t+-+5+cos+2t+-+2+cos+3t+-+cos+4t)
-        var points = [], x, y, t;
-        for (var i = 10; i < 30; i += 0.2) {
-            t = i / Math.PI;
-            x = 16 * Math.pow(Math.sin(t), 3);
-            y = 13 * Math.cos(t) - 5 * Math.cos(2 * t) - 2 * Math.cos(3 * t) - Math.cos(4 * t);
-            points.push(new Point(x, y));
-        }
-        this.points = points;
-        this.length = points.length;
+    var points = [];
+
+    // simple cake outline (rectangle style)
+    points.push(new Point(-30, -10));
+    points.push(new Point(30, -10));
+    points.push(new Point(30, 20));
+    points.push(new Point(-30, 20));
+
+    // top cream layer
+    points.push(new Point(-25, -20));
+    points.push(new Point(25, -20));
+    points.push(new Point(25, -10));
+    points.push(new Point(-25, -10));
+
+    this.points = points;
+    this.length = points.length;
+}
     }
     Heart.prototype = {
         get: function(i, scale) {
@@ -491,7 +496,7 @@
             if (s.scale > 1) {
                 s.tree.removeBloom(s);
             }
-            Heart = Cake;
+            
         },
         draw: function() {
             var s = this, ctx = s.tree.ctx, figure = s.figure;
